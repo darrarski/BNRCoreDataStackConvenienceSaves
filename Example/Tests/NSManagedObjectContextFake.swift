23c697aba9793caa9128a1299446d18a15633a9f
Dariusz Rybicki <darrarski@gmail.com>
2016-04-18 22:56:14 +0200

    NSManagedObjectContextFake class

---
+++ Example/Tests/NSManagedObjectContextFake.swift
@@ -0,0 +1,30 @@
+//
+//  Created by Dariusz Rybicki on 18/04/16.
+//
+
+import CoreData
+
+class NSManagedObjectContextFake: NSManagedObjectContext {
+    var fakeHasChanges = false
+    var shouldThrowOnSave = false
+    
+    private(set) var saveCallsCount = 0
+    private(set) var savesCount = 0
+    private(set) var rollbacksCount = 0
+    
+    override var hasChanges: Bool {
+        return fakeHasChanges
+    }
+    
+    override func save() throws {
+        saveCallsCount += 1
+        if shouldThrowOnSave {
+            throw NSError(domain: "FakeError", code: 0, userInfo: nil)
+        }
+        savesCount += 1
+    }
+    
+    override func rollback() {
+        rollbacksCount += 1
+    }
+}
