a661d614c35502723d551f4e2bdbc7172acb9893
Dariusz Rybicki <darrarski@gmail.com>
2016-04-19 01:50:56 +0200

    Refactoring, added saveWithGroup(group:maxChangedObjects:onError:) method

--- BNRCoreDataStackConvenienceSaves/NSManagedObjectContext+ConvenienceSaves.swift
+++ BNRCoreDataStackConvenienceSaves/NSManagedObjectContext+ConvenienceSaves.swift
@@ -35,20 +35,27 @@ public extension NSManagedObjectContext {
      - parameter onError: A closure that will be called when `save()` function on the `NSManagedObjectContext` produces error.
      */
     public func saveOrRollbackWithGroup(group: dispatch_group_t, maxChangedObjectsCount: Int = 100, onError: (ErrorType) -> ()) {
-        guard changedObjectsCount < maxChangedObjectsCount else {
-            guard self.hasChanges else { return }
-            do { try saveOrRollback() }
-            catch { onError(error) }
-            return
-        }
-        let queue = dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0)
-        dispatch_group_notify(group, queue) {
-            self.performBlockWithGroup(group) {
-                guard self.hasChanges else { return }
-                do { try self.saveOrRollback() }
-                catch { onError(error) }
-            }
-        }
+        saveWithGroup(group,
+                      maxChangedObjectsCount: maxChangedObjectsCount,
+                      usingBlock: { try self.saveOrRollback() },
+                      onError: onError)
+    }
+    
+    /**
+     Convenience method to asynchronously save the `NSManagedObjectContext` if changes are present.
+     The method uses dispatch group to group multiple calls. Save will be delayed, so multiple calls to this method won't cause it
+     untill maximum changed objects count will be exceeded. Save will be always performed on context's queue.
+     If `save()` function on the `NSManagedObjectContext` produces error `onError` closure is called.
+     
+     - parameter group: `dispatch_group_t` that will be used group calls to this method.
+     - parameter maxChangedObjectsCount: Maximum changed objects count, that if exceeded will cause save.
+     - parameter onError: A closure that will be called when `save()` function on the `NSManagedObjectContext` produces error.
+     */
+    public func saveWithGroup(group: dispatch_group_t, maxChangedObjectsCount: Int = 100, onError: (ErrorType) -> ()) {
+        saveWithGroup(group,
+                      maxChangedObjectsCount: maxChangedObjectsCount,
+                      usingBlock: { try self.saveContextAndWait() },
+                      onError: onError)
     }
     
     /**
@@ -58,6 +65,25 @@ public extension NSManagedObjectContext {
      */
     var changedObjectsCount: Int {
         return insertedObjects.count + updatedObjects.count + deletedObjects.count
+    }
+    
+    // MARK: - Helpers
+    
+    private func saveWithGroup(group: dispatch_group_t, maxChangedObjectsCount: Int = 100, usingBlock block: () throws -> (), onError: (ErrorType) -> ()) {
+        guard changedObjectsCount < maxChangedObjectsCount else {
+            guard self.hasChanges else { return }
+            do { try block() }
+            catch { onError(error) }
+            return
+        }
+        let queue = dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0)
+        dispatch_group_notify(group, queue) {
+            self.performBlockWithGroup(group) {
+                guard self.hasChanges else { return }
+                do { try block() }
+                catch { onError(error) }
+            }
+        }
     }
     
     private func performBlockWithGroup(group: dispatch_group_t, block: () -> ()) {
